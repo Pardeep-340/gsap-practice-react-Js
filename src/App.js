9a216ef78de9923de63cd959ebcd19ebf91a8408
dh-pardeep <pardeepjangra514@gmil.com>
2024-06-08 14:37:44 +0530

    create components folder and create a home.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Home />
+    </div>
   );
 }
 
