ae9365d9b5391fe2320d0c38d0e368547fc6f91a
Sumit chouhan <sumitkhan.dhana@gmail.com>
2024-06-08 22:41:28 +0530

    first commit

---
+++ src/components/common/Helper.jsx
@@ -0,0 +1,38 @@
+import myImg from "../../assets/image/png/Image.png";
+export const dataServiceCardData = [
+  {
+    imgUrl: myImg,
+    heading: "Real Estate",
+    para: "Praesent nisi ipsum justo interdum. Feugiat commodo sed malesuada ultricies quis. Praesent nisi ipsum justo interdum. ",
+  },
+  {
+    imgUrl: myImg,
+    heading: "Insurance",
+    para: "Praesent nisi ipsum justo interdum. Feugiat commodo sed malesuada ultricies quis. Praesent nisi ipsum justo interdum. ",
+  },
+  {
+    imgUrl: myImg,
+    heading: "Refinance",
+    para: "Praesent nisi ipsum justo interdum. Feugiat commodo sed malesuada ultricies quis. Praesent nisi ipsum justo interdum. ",
+  },
+  {
+    imgUrl: myImg,
+    heading: "Trade Financing",
+    para: "Praesent nisi ipsum justo interdum. Feugiat commodo sed malesuada ultricies quis. Praesent nisi ipsum justo interdum. ",
+  },
+  {
+    imgUrl: myImg,
+    heading: "Emerging Market Credit",
+    para: "Praesent nisi ipsum justo interdum. Feugiat commodo sed malesuada ultricies quis. Praesent nisi ipsum justo interdum. ",
+  },
+  {
+    imgUrl: myImg,
+    heading: "Revenue Based Financing",
+    para: "Praesent nisi ipsum justo interdum. Feugiat commodo sed malesuada ultricies quis. Praesent nisi ipsum justo interdum. ",
+  },
+  {
+    imgUrl: myImg,
+    heading: "Credit Card Debt Financing",
+    para: "Praesent nisi ipsum justo interdum. Feugiat commodo sed malesuada ultricies quis. Praesent nisi ipsum justo interdum. ",
+  },
+];
